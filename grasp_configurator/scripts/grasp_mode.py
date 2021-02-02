58aeb7ba3d9a3973f54e2d78088d88ec8397e0c4
brdlab <58326412+brdlab@users.noreply.github.com>
2021-02-02 15:16:29 +0800

    Create grasp_mode.py

---
+++ grasp_configurator/scripts/grasp_mode.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import UInt16
+
+from dynamic_reconfigure.server import Server
+from grasp_configurator.cfg import GraspConfig
+
+def callback(config, level):
+	rospy.loginfo("""Reconfigure Request: Grasp mode - {mode_select}, Payload - {payload_select}""". format(**config))
+	return config
+
+if __name__ == '__main__':
+	rospy.init_node("grasp_config", anonymous = False)
+	srv = Server(GraspConfig, callback)
+	rospy.spin()
